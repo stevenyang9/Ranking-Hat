51898a38fe4b796eb8c932bbcbed96c6c69ed46d
Fred Xu <fredxu2015@gmail.com>
2017-03-22 22:32:11 -0400

    fix config.py

--- flask-rankinghat/config.py
+++ flask-rankinghat/config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://ptrk30:dbpasswd@localhost/collegedata'
+SQLALCHEMY_DATABASE_URI = 'postgresql://yourusername:dbpasswd@localhost/collegedata'
 SQLALCHEMY_ECHO = True
 DEBUG = True
