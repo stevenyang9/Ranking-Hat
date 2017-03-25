23d657ee22d5307822587d9ce717c47b71298d0f
Steven Yang <sgyang9@gmail.com>
2017-03-25 19:11:31 -0400

    Integrated backend with frontend

--- flask-rankinghat/app.py
+++ flask-rankinghat/app.py
@@ -2,16 +2,61 @@ from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import models
 import forms
+from sqlalchemy import desc 
 
 app = Flask(__name__)
 app.secret_key = 's3cr3t'
 app.config.from_object('config')
 db = SQLAlchemy(app, session_options={'autocommit': False})
 
+#@app.route('/')
+#def all_schools():
+#    schools = db.session.query(models.school).all()
+#    return render_template('all-schools.html', schools=schools)
+
+# @app.route('/')
+# def all_schools():
+#     schools = db.session.query(models.school).all()
+#     return render_template('all-schools-test.html', schools=schools)
+
+
+
 @app.route('/')
-def all_schools():
+def all_schools_index():
     schools = db.session.query(models.school).all()
-    return render_template('all-schools.html', schools=schools)
+    return render_template('all-schools-index.html', schools=schools)
+
+
+#@app.route('/all-schools')
+#def ranked_schools():
+#    schools = db.session.query(models.school).all()
+#    return render_template('all-schools.html', schools=schools)    
+
+
+@app.route('/all-schools')
+def rankbyadmission_schools():
+    schools = db.session.query(models.school)\
+        .join(models.admissiondata, models.admissiondata.opeid == models.school.opeid)\
+        .add_column(models.school.instnm)\
+        .add_column(models.admissiondata.sat_avg)\
+        .order_by(models.admissiondata.sat_avg.desc()).all()
+    return render_template('all-schools-bysat.html', schools=schools)
+
+
+@app.route('/all-schools-admissionrate')
+def admissionrate():
+    schools = db.session.query(models.school)\
+        .join(models.admissiondata, models.school.opeid == models.admissiondata.opeid)\
+        .add_column(models.school.instnm)\
+        .add_column(models.admissiondata.adm_rate)\
+        .order_by(models.admissiondata.adm_rate).all()
+    return render_template('all-schools-admissionrate.html', outputs=schools)
+
+
+
+
+
+
 
 @app.route('/drinker/<name>')
 def drinker(name):
