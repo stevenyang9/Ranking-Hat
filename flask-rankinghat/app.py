7ef38f402625c3916e9d0c9ccfc0b0689526852d
Fred Xu <fredxu2015@gmail.com>
2017-04-08 16:02:55 -0400

    ranking queries

--- flask-rankinghat/app.py
+++ flask-rankinghat/app.py
@@ -18,19 +18,19 @@ def all_schools_index():
 def rankbyadmission_schools():
     schools = db.session.query(models.school)\
         .join(models.admissiondata, models.admissiondata.opeid == models.school.opeid)\
+        .join(models.graddata, models.graddata.opeid == models.school.opeid)\
         .add_column(models.school.instnm)\
         .add_column(models.admissiondata.sat_avg)\
-        .order_by(models.admissiondata.sat_avg.desc()).limit(25).all()
+        .add_column(models.admissiondata.adm_rate)\
+        .add_column(models.graddata.c150_4)\
+        .order_by(models.admissiondata.sat_avg).limit(25).all()
     return render_template('all-schools-bysat.html', schools=schools)
 
 
 @app.route('/all-schools-admissionrate')
 def admissionrate():
-    schools = db.session.query(models.school)\
-        .join(models.admissiondata, models.school.opeid == models.admissiondata.opeid)\
-        .add_column(models.school.instnm)\
-        .add_column(models.admissiondata.adm_rate)\
-        .order_by(models.admissiondata.adm_rate).limit(25).all()
+    s="schools = db.session.query(models.school).join(models.admissiondata, models.school.opeid == models.admissiondata.opeid).add_column(models.school.instnm).add_column(models.admissiondata.adm_rate).order_by(models.admissiondata.adm_rate).limit(25).all()"
+    exec(s)
     return render_template('all-schools-admissionrate.html', outputs=schools)
 
 
@@ -40,3 +40,4 @@ def pluralize(number, singular='', plural='s'):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
